65dcd381da19777b94da28097b1d55bb31f201c5
Marius Nuennerich <marius@nuenneri.ch>
2011-01-19 15:02:14 +0100

    add affil, arrival, depart to hotel report

--- sql/views/view_hotel_list.sql
+++ sql/views/view_hotel_list.sql
@@ -3,10 +3,13 @@ CREATE OR REPLACE VIEW view_hotel_list AS
   SELECT name,
     ( SELECT name FROM view_conference_person WHERE conference_person_id = accommodation_mate_id) AS room_mate,
     roomtype,
-    accommodation_name
+    accommodation_name,
+    affiliation,
+    arrival_date,
+    departure_date
   FROM view_conference_person
       JOIN conference_person_travel USING (conference_person_id)
       JOIN custom.custom_conference_person USING (person_id)
-  ORDER BY accommodation_name
+  ORDER BY affiliation
 ;
 
