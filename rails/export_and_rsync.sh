9a3bda3a8095ab31e70e41efb4f00d6483b7a510
root <root@penta.fosdem.org>
2013-10-30 17:19:00 +0100

    public: remove fosdem howto

--- rails/export_and_rsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-F="FOSDEM12"
-
-/bin/rm -rf "/pentabarf/rails/tmp/fosdem-export/$F/" && \
-cd /pentabarf/rails && \
-./script/fosdem-export "$F" && \
-rsync -azd --ignore-times -e 'ssh -i /home/pentabarf/.ssh/apeiron_id_rsa -l pentabarf' "/pentabarf/rails/tmp/fosdem-export/$F/" apeiron.fosdem.org:"/home/services/pentabarf/tmp/html-export/$F/"
