f2d8ce05fc2456ae38a4dbd83707411b3a698754
Alasdair G Kergon <agk@fosdem.org>
2015-09-16 02:54:13 +0100

    sql: Fix activate_account ambiguities.

--- sql/functions/auth/activate_account.sql
+++ sql/functions/auth/activate_account.sql
@@ -4,19 +4,20 @@ CREATE OR REPLACE FUNCTION auth.activate_account( activation_string char(64)) RE
     cur_activation RECORD;
     cur_person_id INTEGER;
     activation_interval INTERVAL;
+    p_activation_string ALIAS FOR $1;
   BEGIN
     activation_interval = '-7 day';
     DELETE FROM auth.account WHERE account_id IN ( SELECT account_id FROM auth.account_activation WHERE account_creation < now() + activation_interval );
-    SELECT INTO cur_activation account_id, conference_id FROM auth.account_activation WHERE account_activation.activation_string = activation_string;
+    SELECT INTO cur_activation account_id, conference_id FROM auth.account_activation WHERE account_activation.activation_string = p_activation_string;
     IF FOUND THEN
       INSERT INTO auth.account_role(account_id, role) VALUES (cur_activation.account_id, 'submitter');
       SELECT INTO cur_person_id nextval(pg_get_serial_sequence('base.person','person_id'));
       INSERT INTO person(person_id,nickname,email) SELECT cur_person_id,login_name,email FROM auth.account WHERE account.account_id = cur_activation.account_id;
       UPDATE auth.account SET person_id = cur_person_id WHERE account_id = cur_activation.account_id;
       INSERT INTO auth.account_settings(account_id,current_conference_id) VALUES (cur_activation.account_id, cur_activation.conference_id);
-      DELETE FROM auth.account_activation WHERE account_activation.activation_string = activation_string;
+      DELETE FROM auth.account_activation WHERE account_activation.activation_string = p_activation_string;
     ELSE
-      PERFORM 1 FROM log.account_activation WHERE account_activation.activation_string = activation_string AND account_activation.log_operation = 'D';
+      PERFORM 1 FROM log.account_activation WHERE account_activation.activation_string = p_activation_string AND account_activation.log_operation = 'D';
       IF FOUND THEN
         RAISE EXCEPTION 'Your account has already been activated.';
       ELSE
